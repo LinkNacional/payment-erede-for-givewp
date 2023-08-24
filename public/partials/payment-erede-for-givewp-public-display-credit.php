228db9e1787598fd41db84cac836045659c0e6fa
Emanuel Lopes <lopes.emanuel@linknacional.com>
2023-08-24 21:40:58 +0000

    feat: Front-end credit card field implementation

--- public/partials/payment-erede-for-givewp-public-display-credit.php
+++ public/partials/payment-erede-for-givewp-public-display-credit.php
@@ -42,7 +42,88 @@ do_action('give_before_cc_fields', $args['form_id']);
         exit;
     }
 ?>
-    <h1>Hello World!</h1>
+    <!-- CARD NUMBER INPUT -->
+    <div id="give-card-number-wrap" class="form-row form-row-two-thirds form-row-responsive give-lkn-cielo-api-cc-field-wrap">
+        <label for="card_number-<?php esc_attr_e($args['form_id']); ?>" class="give-label">
+            Número do cartão
+            <span class="give-required-indicator">*</span>
+            <span class="give-tooltip hint--top hint--medium hint--bounce" aria-label="Normalmente possui 16 digitos na frente do seu cartão de crédito." rel="tooltip"><i class="give-icon give-icon-question"></i></span>
+        </label>
+        <input
+            type="tel"
+            autocomplete="off"
+            name="lkn_erede_credit_card_number"
+            id="card_number-<?php esc_attr_e($args['form_id']); ?>"
+            class="card-number give-input required"
+            placeholder="Número do cartão"
+            required=""
+            aria-required="true"
+        />
+    </div>
+
+    <!-- CARD EXPIRY INPUT -->
+    <div id="give-card-expiration-wrap" class="card-expiration form-row form-row-one-third form-row-responsive give-lkn-cielo-api-cc-field-wrap">
+        <label for="give-card-expiration-field-<?php esc_attr_e($args['form_id']); ?>" class="give-label">
+            Expiração
+            <span class="give-required-indicator">*</span>
+            <span class="give-tooltip give-icon give-icon-question"
+                data-tooltip="A data de expiração do cartão de crédito, geralmente na frente do cartão."></span>
+        </label>
+        <input
+            type="tel"
+            autocomplete="off"
+            name="lkn_erede_credit_card_expiry"
+            id="card_expiry-<?php esc_attr_e($args['form_id']); ?>"
+            class="card-expiry give-input required"
+            placeholder="MM / AAAA"
+            required=""
+            aria-required="true"
+        />
+    </div>
+
+    <!-- CARD HOLDER INPUT -->
+    <div id="give-card-name-wrap" class="form-row form-row-two-thirds form-row-responsive">
+        <label for="give-card-name-field-<?php esc_attr_e($args['form_id']); ?>" class="give-label">
+            Nome do títular do cartão
+            <span class="give-required-indicator">*</span>
+            <span class="give-tooltip give-icon give-icon-question"
+                data-tooltip="O nome do titular da conta do cartão de crédito.">
+            </span>
+        </label>
+        <input
+            type="text"
+            autocomplete="off"
+            id="give-card-name-field-<?php esc_attr_e($args['form_id']); ?>"
+            name="lkn_erede_credit_card_name"
+            class="card-name give-input required"
+            placeholder="Nome do titular do cartão"
+            required=""
+            aria-required="true"
+        />
+    </div>
+
+    <!-- CARD CVV INPUT -->
+    <div id="give-card-cvc-wrap" class="form-row form-row-one-third form-row-responsive give-lkn-cielo-api-cc-field-wrap">
+        <label for="give-card-cvc-field-<?php esc_attr_e($args['form_id']); ?>" class="give-label">
+            CVV
+            <span class="give-required-indicator">*</span>
+            <span class="give-tooltip give-icon give-icon-question"
+                data-tooltip="São os 3 ou 4 dígitos que estão atrás do seu cartão de crédito."></span>
+        </label>
+        <div id="give-card-cvc-field-<?php esc_attr_e($args['form_id']); ?>" class="input empty give-lkn-cielo-api-cc-field give-lkn-cielo-api-card-cvc-field"></div>
+        <input
+            type="tel"
+            size="4"
+            maxlength="4"
+            autocomplete="off"
+            name="lkn_erede_credit_card_cvc"
+            id="card_cvc-<?php esc_attr_e($args['form_id']); ?>"
+            class="give-input required"
+            placeholder="CVV"
+            required=""
+            aria-required="true"
+        />
+    </div>
 <?php
 do_action('give_after_cc_expiration', $args['form_id'], $args['settings']);
 
