e47d34336863c5b03efa6f24882aa1231030a108
Emanuel Lopes <lopes.emanuel@linknacional.com>
2023-08-29 20:44:56 +0000

    improvement: Load optimization and add better gateway detection

--- public/js/payment-erede-for-givewp-debit-3ds.js
+++ public/js/payment-erede-for-givewp-debit-3ds.js
@@ -2,9 +2,31 @@
 (function ($) {
   'use strict'
 
-  $(window).on('load', () => {
+  $(window).on('load', () => lknLoadEredeDebit3DS())
+
+  function lknLoadEredeDebit3DS () {
     const iframe = document.getElementsByName('give-embed-form')[0]
     const giveForm = $('.give-form')
+
+    if (iframe) {
+      lknSetInputsEredeDebit3DS('iframe')
+      const gatewayList = iframe.contentDocument.getElementById('give-gateway-radio-list')
+      if (gatewayList) {
+        gatewayList.addEventListener('click', () => lknSetInputsEredeDebit3DS('iframe'))
+      }
+    } else if (giveForm.length) {
+      lknSetInputsEredeDebit3DS('legacy')
+      const gatewayList = $('#give-gateway-radio-list')
+      if (gatewayList.length) {
+        gatewayList.on('click', () => lknSetInputsEredeDebit3DS('legacy'))
+      }
+    }
+  }
+
+  function lknSetInputsEredeDebit3DS (typeForm, count = 0) {
+    count++
+
+    const iframe = document.getElementsByName('give-embed-form')[0]
 
     const language = window.navigator.language.slice(0, 2)
     const height = screen.height
@@ -14,20 +36,64 @@
     const date = new Date()
     const timezoneOffset = date.getTimezoneOffset()
 
-    if (iframe) {
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_user_agent')[0].value = userAgent
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_color')[0].value = colorDepth
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_lang')[0].value = language
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_height')[0].value = height
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_width')[0].value = width
-      iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_timezone')[0].value = timezoneOffset
-    } else if (giveForm.length) {
-      $('[name="lkn_erede_debit_3ds_user_agent"]').attr('value', userAgent)
-      $('[name="lkn_erede_debit_3ds_device_color"]').attr('value', colorDepth)
-      $('[name="lkn_erede_debit_3ds_lang"]').attr('value', language)
-      $('[name="lkn_erede_debit_3ds_device_height"]').attr('value', height)
-      $('[name="lkn_erede_debit_3ds_device_width"]').attr('value', width)
-      $('[name="lkn_erede_debit_3ds_timezone"]').attr('value', timezoneOffset)
+    if (typeForm === 'iframe') {
+      const userAgentInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_user_agent')[0]
+      const deviceColorInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_color')[0]
+      const langInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_lang')[0]
+      const heightInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_height')[0]
+      const widthInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_device_width')[0]
+      const timezoneInput = iframe.contentDocument.getElementsByName('lkn_erede_debit_3ds_timezone')[0]
+
+      if (
+        userAgentInput &&
+        deviceColorInput &&
+        langInput &&
+        heightInput &&
+        widthInput &&
+        timezoneInput
+      ) {
+        userAgentInput.value = userAgent
+        deviceColorInput.value = colorDepth
+        langInput.value = language
+        heightInput.value = height
+        widthInput.value = width
+        timezoneInput.value = timezoneOffset
+
+        return true
+      }
+    } else {
+      const userAgentInput = $('[name="lkn_erede_debit_3ds_user_agent"]')
+      const deviceColorInput = $('[name="lkn_erede_debit_3ds_device_color"]')
+      const langInput = $('[name="lkn_erede_debit_3ds_lang"]')
+      const heightInput = $('[name="lkn_erede_debit_3ds_device_height"]')
+      const widthInput = $('[name="lkn_erede_debit_3ds_device_width"]')
+      const timezoneInput = $('[name="lkn_erede_debit_3ds_timezone"]')
+
+      if (
+        userAgentInput.length &&
+        deviceColorInput.length &&
+        langInput.length &&
+        heightInput.length &&
+        widthInput.length &&
+        timezoneInput.length
+      ) {
+        userAgentInput.attr('value', userAgent)
+        deviceColorInput.attr('value', colorDepth)
+        langInput.attr('value', language)
+        heightInput.attr('value', height)
+        widthInput.attr('value', width)
+        timezoneInput.attr('value', timezoneOffset)
+
+        return true
+      }
     }
-  })
+
+    // Only run 4 times
+    if (count > 4) {
+      return false
+    }
+
+    // Run again if inputs are not found
+    setTimeout(() => lknSetInputsEredeDebit3DS(typeForm, count), 1000)
+  }
 })(jQuery)
